6ce2b0fa1a0afb380cdbf081096950a417bcd7be
KTOY <sugarmedamarbat@gmail.com>
2026-08-08 09:31:57 +0800

    Добавил для mikrotik

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Network automation server is working!")
+from netmiko import ConnectHandler
+router = {
+    'device_type': 'miktotik_routeros',
+    'host': '192.168.24.11',
+    'username': 'admin',
+    'password': '123456'
+}
+connection = ConnectHandler(**router)
+print(connection.send_command("/system identity print"))
+connection.disconnect()
