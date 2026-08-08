c1841b1092537fc62b7eca81ccc6a5e3ec26e181
KTOY <sugarmedamarbat@gmail.com>
2026-08-08 13:36:10 +0800

    ip хувиарилалт

--- ip_port.py
+++ ip_port.py
@@ -4,10 +4,14 @@ router = {
     'host': '192.168.24.11',
     'username': 'admin',
     'password': '123456'
-}
+    }
+
 connection = ConnectHandler(**router)
 print(connection.send_command("/system identity print"))
-command = ["/interface print", "/ip address print", "/ip route print", "/ip arp print"]
-for cmd in command:
-    print(connection.send_command(cmd))
-connection.disconnect()
+
+data = {
+    'ip_address': '192.168.23.1/24',
+    'port': 'ether1'  
+    }
+for key, value in data.items():
+    print(connection.send_command(f"/ip address add address={data['ip_address']}, interface={data['port']}"))
